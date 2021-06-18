e8acce1c740770e454b32796a876e7d2ab8ba10e
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-06-18 18:01:32 -0300

    autenticaçao com jwt parte 1

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,82 @@
+import Router from 'next/router';
+import { parseCookies, setCookie } from 'nookies';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+import { api } from '../services/api';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[]
+}
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User | undefined;
+  isAuthenticated: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+
+const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>()
+  const isAuthenticated= !!user;
+
+  useEffect(()=>{
+    const { 'auth.token': token } = parseCookies();
+    if(token){
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+        setUser({
+          email,
+          permissions,
+          roles
+        })
+      })
+    }
+  }, [])
+
+  async function signIn({ email, password }: SignInCredentials){
+    try {
+      const response = await api.post('sessions', {
+        email, password
+      })
+      const { token, refreshToken, permissions, roles } = response.data
+      setCookie(undefined, 'auth.token', token, {
+        maxAge: 60*60*24*30,
+        path: '/'
+      })
+      setCookie(undefined, 'auth.refreshToken', refreshToken, {
+        maxAge: 60*60*24*30,
+        path: '/'
+      })
+      setUser({
+        email,
+        permissions,
+        roles
+      })
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+      Router.push('/dashboard')
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{signIn, isAuthenticated, user}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext);
---
+++ pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+export default function Dashboard() {
+  
+  return (
+    <div className={styles.container}>
+      <h1>dashboard</h1>
+    </div>
+  )
+}
